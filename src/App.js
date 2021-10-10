4a8d594264906252af94eaa2a906473838d05097
ervinanovlianti <ervinanov03@gmail.com>
2021-10-10 11:03:09 -0700

    Frontend React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Beranda from './components/Beranda';
+import Navbar from './components/Navbar';
+import ManajemenBuku from './components/ManajemenBuku';
+import { useState } from 'react';
 
 function App() {
+  const [books, setBooks] = useState([
+    { _id: 1, judul: "Laskar Pelangi", pengarang: "Andrea Hirata", harga: 80000, stok: 7 },
+    { _id: 2, judul: "Bumi", pengarang: "Tere Liye", harga: 85000, stok: 5 },
+  ]);
+  function storeData(inputBook){
+    console.log(inputBook);
+    alert("Data Berhasil Ditambahkan");
+  }
+  function updateData(inputBook) {
+    console.log(inputBook);
+    alert("Data Berhasil diperbaharui!")
+  }
+  function deleteData(book) {
+    console.log(book);
+    alert("Data Berhasil Dihapus!")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+
+        <Switch>
+          <Route path="/" exact>
+            <Beranda bookList={books}/>
+          </Route>
+
+          <Route path="/manajemen-buku" exact>
+            <ManajemenBuku 
+            bookList={books} 
+            store={storeData} 
+            update={updateData}
+            remove={deleteData}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
